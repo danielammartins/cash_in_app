41e873ef35b8be68362a8c70c4dcee749e273b48
Daniela Martins <57196374+danielammartins@users.noreply.github.com>
2021-12-29 17:17:57 +0000

    added search function, fixed id bug

--- app-backend/routes/api.php
+++ app-backend/routes/api.php
@@ -8,24 +8,14 @@ use App\Models\Categories;
 use App\Http\Controllers\ExpensesController;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-/*
     Public Routes
 */
+
 
 //Route::get('/expenses', [ExpensesController::class, 'index']);
 //Route::post('/expenses', [ExpensesController::class, 'store']);
 
-//FIX maneira preguiçosa se não ter de fazer cada rota individualmente
+//FIXME this is lazy
 Route::resource('expenses', ExpensesController::class);
 
 /*
@@ -34,4 +24,3 @@ Route::resource('expenses', ExpensesController::class);
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/expenses/search/{name}', [ExpensesController::class, 'search']);
 });
-
